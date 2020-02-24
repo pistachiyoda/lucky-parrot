4006e7223c827016906ec022e1c2c1edc76d83e2
mai <fujisawamai21@gmail.com>
2020-02-24 15:45:15 +0900

    images

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Today's Your Lucky Parrot</h1>
+    <img alt="parrot" :src="path" />
+    <span>{{ parrot_name }}</span>
+    <button @click="show_random_parrot">ランダムなparrotを表示</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data: function() {
+    return {
+      path: "",
+      parrot_name: "test"
+    };
+  },
+  methods: {
+    show_random_parrot: async function() {
+      const response = await fetch("/list.txt");
+      const parrot_list = await response.text();
+      const parrot_array = parrot_list.split("\n");
+      function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+      const path = parrot_array[getRandomInt(parrot_array.length)];
+      this.path = path;
+      this.parrot_name = path.slice(8, -4);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
